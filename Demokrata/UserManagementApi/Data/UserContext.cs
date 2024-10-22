2d5d77481e9626ae541c9910bb27f919b56ebb11
felipeosouri <jfosorio@outlook.com>
2024-10-22 18:56:33 -0500

    Create and configure context

---
+++ Demokrata/UserManagementApi/Data/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementApi.Models;
+
+namespace UserManagementApi.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Demokrata/UserManagementApi/Program.cs
+++ Demokrata/UserManagementApi/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using UserManagementApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
